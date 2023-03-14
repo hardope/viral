a3066098efd51208ae2389630401a342adbbda86
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-03-14 11:55:52 +0100

    Updates

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
      path('', views.feed, name="feed"),
+     path('fetch_posts', views.fetch_posts, name="fetch_posts"),
+     path('new_post', views.new_post, name="new_post"),
+     path('notification', views.notification, name="notification"),
+
+
      path('login', views.login_view, name="login"),
      path('register', views.register, name="register"),
      path('logout', views.logout_view, name="logout"),
