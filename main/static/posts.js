66543f532119d47cc4952f876d1167611670a75b
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-03-16 15:29:48 +0100

    'Updated

--- main/static/posts.js
+++ main/static/posts.js
@@ -9,7 +9,7 @@ function load() {
     request.open("GET", url + "/fetch_posts");
     request.send();
     request.onload = () => {
-       if (request.status === 200) {
+        if (request.status === 200) {
             let body = document.querySelector('#body');
             for (let obj of JSON.parse(request.response)) {
                 var name_container = document.createElement('div')
@@ -22,8 +22,8 @@ function load() {
                 name_link.appendChild(pic)
                 var name = document.createElement('div');
                 name.setAttribute('class', 'p')
-                name.textContent = obj.name;
-                name_link.setAttribute('href', "/profile/" + obj.name)
+                name.textContent = obj.user;
+                name_link.setAttribute('href', "/profile/" + obj.user)
                 name.setAttribute('style', 'margin-left: 10px; margin-top: 30px;')
                 name_link.appendChild(name)
                 name_container.appendChild(name_link)
@@ -31,31 +31,33 @@ function load() {
                 var time = document.createElement('p');
                 time.setAttribute('style', 'margin-top: -10px; margin-left: 30px; font-size: 15px !important;')
                 time.setAttribute('class', 'a')
-                time.textContent = get_time(obj.time);
+                time.textContent = get_time(obj.created_at);
                 body.appendChild(time);
                 var tag = document.createElement('div');
-                for (let line of obj.content.split("\n")){
+                for (let line of obj.article.split("\n")){
                     let par = document.createElement('div')
                     par.textContent = line
                     tag.appendChild(par)
                 }
                 tag.setAttribute('class', 'div')
                 body.appendChild(tag);
-                if (obj.media === "pic"){
-                    let link = document.createElement('a');
-                    link.setAttribute("href", '/static/'+ obj.id +'.jpg')
-                    var pic = document.createElement('img');
-                    pic.setAttribute('src', '/static/'+ obj.id +'.jpg')
-                    link.appendChild(pic)
-                    body.appendChild(link)
-                } else if (obj.media === "video") {
-                    var video = document.createElement("video")
-                    video.setAttribute('src', "/static/" + obj.id + ".mp4")
-                    video.setAttribute('controls', "")
-                    video.setAttribute('prelolad', "metadata")
-
-                    video.setAttribute('loop', "")
-                    body.appendChild(video)
+                if (obj.media != "empty"){
+                    if (obj.media == "mp4") {
+                        var video = document.createElement("video")
+                        video.setAttribute('src', '/static/posts/'+ obj.id +'.' + obj.media)
+                        video.setAttribute('controls', "")
+                        video.setAttribute('prelolad', "metadata")
+                        video.setAttribute('loop', "")
+                        body.appendChild(video)
+                    } else {
+                        let link = document.createElement('a');
+                        link.setAttribute("href", '/static/posts/'+ obj.id +'.' + obj.media)
+                        var pic = document.createElement('img');
+                        pic.setAttribute('src', '/static/posts/'+ obj.id + '.' + obj.media)
+                        link.appendChild(pic)
+                        body.appendChild(link)
+                    }
+                    
                 }
                 var container = document.createElement('div');
                 container.setAttribute('class', 'container')
@@ -82,7 +84,7 @@ function load() {
                 v_like.setAttribute('onclick', 'view_likes(' + obj.id + ')')
                 container.appendChild(v_like)
                 body.appendChild(container)
-
+           
                 a+=1
             }
        }
